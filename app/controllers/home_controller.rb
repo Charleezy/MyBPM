7b5d9da2058d48ef572d4c776b28f03fb48f0654
Mattia Padovani <padovani.mattia@gmail.com>
2014-03-21 16:24:19 -0500

    removing unnecessary view logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,12 @@ class HomeController < ApplicationController
   def about
     render 'about'
   end
+
+  def setup_side_nav_links
+    super
+    @subnav_links = [
+      {:text => "Main", :url => '/'},
+      {:text => "About", :url => '/about'}
+    ]
+  end
 end
